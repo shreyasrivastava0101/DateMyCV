7c5106d3ae2737e3b52ee484ac794ec6028bad9b
shreyasrivastava0101 <shreyasrivastava8078@gmail.com>
2026-09-16 04:45:34 +0530

    fix: update password hashing middleware

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -84,16 +84,15 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  }
+  },
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    return next();
+    return;
   }
 
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
